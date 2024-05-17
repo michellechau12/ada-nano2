07b762252ac6cb2fa8132bab8e786547c3dfd9b5
michellechau12 <micheelechau@gmail.com>
2024-05-17 11:29:28 +0700

    firstupdate

---
+++ Nano 2 iPad/Nano_2_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_2_iPadApp.swift
+//  Nano 2 iPad
+//
+//  Created by Michelle Chau on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano_2_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
